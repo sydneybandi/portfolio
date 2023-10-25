5b8ec453851579446a8256ad3dae069d9a54cd14
Sydney Bandi <45433877+sydneybandi@users.noreply.github.com>
2023-10-25 04:01:22 +0300

    Update on Experience

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to false if you don't want Splash screen.
+  isSplash: false, // Change this to false if you don't want Splash screen.
 };
 
 //SEO Related settings
@@ -24,7 +24,7 @@ const greeting = {
   nickname: "Data Engineer | Data Scientist",
   subTitle:
     "Motivate, teamwork-oriented, and responsible Data Analyst with significant experience in increasing comprehension of reports and presentations by the average professional. Highly skilled, data-driven and possessing a professional certification in Data and Product analytics .",
-  resumeLink: "https://bit.ly/3gRTr3Q",
+  resumeLink: "http://bit.ly/3GcR5a6",
   portfolio_repository: "https://github.com/sydneybandi",
   githubProfile: "https://github.com/sydneybandi",
 };
@@ -175,7 +175,7 @@ const skills = {
       ],
     },
     {
-      title: "Web Development",
+      title: "Full Stack Development",
       fileName: "FullStackImg",
       skills: [
         "⚡ Building responsive website front end using Django",
@@ -257,9 +257,13 @@ const skills = {
       ],
     },
     {
-      title: "Digital Marketing and SEO Optimisation",
+      title: "UI/UX Design",
       fileName: "DesignImg",
-      skills: [],
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile and web applications",
+        "⚡ Customizing logo designs and building logos from scratch",
+        "⚡ Creating the flow of application functionalities to optimize user experience",
+      ],
       softwareSkills: [
         {
           skillName: "Adobe XD",
@@ -315,7 +319,7 @@ const degrees = {
       subtitle: "BSc. in Information Technology",
       logo_path: "MMU_logo.png",
       alt_name: "MMU",
-      duration: "2018 - Present",
+      duration: "2018 - 2022",
       descriptions: [
         "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
         "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
@@ -326,9 +330,9 @@ const degrees = {
     {
       title: "Africa Institute of Research and Development College",
       subtitle: "Certificate in Computer Programming",
-      logo_path: "nptel_logo.png",
+      logo_path: "airads.png",
       alt_name: "AIRADS",
-      duration: "2017",
+      duration: "2018",
       descriptions: [
         "Learned how to:",
         "⚡ Use foundational programming and software development knowledge and skills to develop efficient software applications",
@@ -344,6 +348,15 @@ const degrees = {
 const certifications = {
   certifications: [
     {
+      title: "Google Cybersecurity Professional Certificate",
+      subtitle: "- Google",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://coursera.org/share/90c6059b85789bfb446f2581d34110a1",
+      alt_name: "Google",
+      color_code: "#0C9D5899",
+    },
+    {
       title: "AWS Certified Data Analytics - Specialty",
       subtitle: "- AWS",
       logo_path: "stanford_logo.png",
@@ -355,7 +368,8 @@ const certifications = {
       title: "Product Analytics",
       subtitle: "- product school",
       logo_path: "deeplearning_ai_logo.png",
-      certificate_link: "",
+      certificate_link:
+        "https://drive.google.com/file/d/1OlTpKRiX77AW3DhwxkG7X8rxJAZ6w7d4/view",
       alt_name: "product",
       color_code: "#00000099",
     },
@@ -406,14 +420,38 @@ const experience = {
       title: "Work",
       experiences: [
         {
+          title: "API Developer",
+          company: "World Vision Kenya",
+          company_url: "https://www.wvi.org/kenya",
+          logo_path: "WV.png",
+          duration: "Sep 2022 - Present",
+          location: "Karen,Kenya",
+          description: [
+            "1) Project Planning: Led project planning, met timelines, and achieved budget goals.  (2) API Portfolio Management: Ensured efficient use of existing APIs.  (3) Design: Translated business requirements into technical specifications.  (4) Code and Test: Ensured code compliance with design and effective testing.  (5) Deployment: Contributed to successful code deployment.  (6) Maintenance (Break/Fix): Addressed minimal issues for reliable applications.  (7) Production Support: Provided Level 3 support, minimizing errors.  (8) Documentation/User Guides: Created clear, user-friendly documentation.",
+          ],
+          color: "#0879bf",
+        },
+        {
+          title: "Data Analyst",
+          company: "ControlTech Limited",
+          company_url: "https://www.controltech-ea.com/",
+          logo_path: "controltech.png",
+          duration: "May 2022 - Aug 2022",
+          location: "Nairobi,Kenya",
+          description: [
+            "(1) Data Collection and Integration: Collected, cleaned, and integrated data from various sources, including asset tracking devices, live streaming, and historical data. (2) Data Analysis: Analyzed data to identify trends, patterns, and anomalies that provided valuable insights for the company. (3)  Visualization: Created clear and informative data visualizations, such as charts and graphs, to help stakeholders understand the data more easily.  (4) Real-time Monitoring: Developed systems or dashboards for real-time tracking of assets and live streaming data, allowing for immediate responses to events or issues.  (5) Historical Analysis: Performed historical data analysis to identify long-term trends and assessed the performance of assets and systems over time.  (6) Reporting: Generated regular and ad-hoc reports for management and other stakeholders to aid in decision-making.  (7) Predictive Analytics: Utilized data to build predictive models that anticipated future events or asset behavior, helping with proactive maintenance and decision-making.  (8)  Security: Ensured the security and privacy of sensitive data, especially when dealing with location and video data.  (9) Collaboration: Worked closely with other teams within the organization, such as IT, engineering, and operations, to understand their data needs and provide insights.",
+          ],
+          color: "#0879bf",
+        },
+        {
           title: "Data Scientist",
           company: "mTek-Services",
           company_url: "https://mtek-services.com/",
-          logo_path: "ibm_logo.png",
+          logo_path: "mtek.png",
           duration: "Feb 2021 - April 2022",
           location: "Nairobi,Kenya",
           description: [
-            "1) Contributed to the intergration of hubspot CRM for sales force automation.This contributed to the increased the number of sales by 20%      (2)Introduced Mixpanel for product analytics that enabled the company to see where there product works well and where it doesn't (3)Introduced and Integrated data studio to Automate and schedule web analytics reports that was consumed by team members and the management.",
+            "As a Data Scientist at mTek-Services, I played a pivotal role in enhancing our B2C digital online insurance platform. My responsibilities included leveraging data-driven insights to drive innovation and improve decision-making. Notable accomplishments during my tenure include:  (1) Sales Force Automation: Successfully integrated HubSpot CRM for sales force automation, resulting in a remarkable 20% increase in sales. This initiative streamlined our sales processes, leading to a more efficient and productive sales team.  (2) Product Analytics Enhancement: Introduced Mixpanel for in-depth product analytics, providing valuable insights into the performance of our offerings. This allowed the company to pinpoint areas of success and identify opportunities for improvement.  (3) Data Reporting Automation: Implemented and integrated Data Studio to automate and schedule web analytics reports. These reports were instrumental in aiding team members and management to make data-informed decisions efficiently.   My role involved not only these accomplishments but also encompassed responsibilities such as data analysis, modeling, and data-driven strategy development. I consistently applied data science methodologies to empower our platform and drive growth. My contributions were pivotal in maintaining mTek-Services' reputation as a leader in the B2C online insurance sector.",
           ],
           color: "#0879bf",
         },
@@ -421,11 +459,11 @@ const experience = {
           title: "Data Analyst",
           company: "DataSteerz",
           company_url: "https://www.datasteerz.com/",
-          logo_path: "gcp_logo.png",
+          logo_path: "datasteerz.png",
           duration: "June 2020 - Dec 2020",
           location: "Eldoret,Kenya",
           description:
-            "Through lexical analysis and syntactic analysis (parsing), I transformed unstructured data into structured data that is easier to read, understand and use.This enabled the company to scrape data (jobs) from the internet and provide it to clients",
+            "Utilized advanced lexical and syntactic analysis techniques to convert unstructured data into a highly organized and comprehensible format. This pivotal transformation empowered the company to extract valuable job data from the web, enhancing our ability to deliver tailored solutions to clients.",
           color: "#9b1578",
         },
       ],
@@ -434,14 +472,25 @@ const experience = {
       title: "Internships",
       experiences: [
         {
+          title: "Data Analyst and Modeler Intern",
+          company: "World Vision Kenya",
+          company_url: "https://www.wvi.org/kenya",
+          logo_path: "WV.png",
+          duration: "Sep 2022 - Aug 2023",
+          location: "Karen,Kenya",
+          description:
+            "As a Data Analyst and Modeler Intern at World Vision, I played a vital role in optimizing data models and harnessing the power of data to drive informed decision-making. My key responsibilities included:  (1) Strategic Development: Actively contributed to the development of new, sophisticated Power BI data models, enabling the organization to extract valuable insights from its vast data resources. These models served as the backbone of data-driven strategies.  (2) Model Enhancement: Collaborated with the team to enhance and refine existing data models. By incorporating advanced techniques and best practices, I improved data accuracy, accessibility, and usability, ultimately maximizing the value of these models.  (3) Documentation Expertise: Skillfully created comprehensive documentation for current data models, ensuring transparency and ease of understanding for team members and stakeholders. This documentation facilitated efficient model maintenance and knowledge sharing across the organization.  My internship at World Vision allowed me to gain hands-on experience in data modeling, analysis, and documentation, reinforcing my commitment to leveraging data for positive social impact.",
+          color: "#0071C5",
+        },
+        {
           title: "Data Analyst Intern",
           company: "mTek-Services",
           company_url: "https://mtek-services.com/",
-          logo_path: "intel_logo.jpg",
+          logo_path: "mtek.png",
           duration: "Feb 2021 - May 2021",
           location: "Nairobi,Kenya",
           description:
-            "1) Preparing final analysis reports for the management to understand the data-analysis steps, enabling them to take important decisions based on various facts and trends (2)Using statistical tools to identify, analyze, and interpret patterns and trends in complex data sets that could be helpful for the diagnosis and prediction (3)Using automated tools to extract data from primary and secondary sources",
+            "At mTek-Services, as a data analyst intern, I played a pivotal role in harnessing data-driven insights to empower decision-making and enhance the efficiency of our paperless B2C insurance platform. My responsibilities included:  (1)Advanced Data Analysis: I spearheaded the development of final analysis reports for the management, distilling complex data-analysis steps into actionable insights. This enabled the leadership team to make well-informed decisions based on a comprehensive understanding of various facts and emerging trends.  (2)Statistical Expertise: I leveraged cutting-edge statistical tools to identify, analyze, and interpret intricate patterns and trends within extensive data sets. This analytical prowess was instrumental in diagnosing issues, predicting outcomes, and optimizing our platform's performance.  (3)Data Extraction Automation: I streamlined data extraction processes by implementing automated tools, extracting valuable information from both primary and secondary sources. This automation facilitated rapid access to critical data, supporting our platform's seamless operation.  My contribution as a data analyst intern at mTek not only enriched our data-driven decision-making but also fostered an environment of continuous improvement, contributing to the platform's ongoing success.",
           color: "#0071C5",
         },
       ],
@@ -511,7 +560,7 @@ const publications = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "sydneybandi.jpeg",
+    profile_image_path: "Sydney.jpg",
     description:
       "I am available on the listed social media platforms. You can message me, I will reply within 24 hours. I can help you with Data Analytics ,Business Intelligence, Building and Automating Data Pipelines , API integrations, Building interactive web appications using R-Shiny and Digital Marketing.",
   },
@@ -519,7 +568,7 @@ const contactPageData = {
     title: "Articles",
     subtitle:
       "For individual fundamental empowerment, I am working to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://twitter.com/bandi_sydney",
+    link: "https://twitter.com/bandi_sydney/likes",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
